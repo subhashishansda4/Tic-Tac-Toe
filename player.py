93815fe399d673560769facf9166acb111938600
Subhashis Hansda <subhashishansda2@gmail.com>
2022-03-04 16:23:41 +0530

    added AI using minimax algorithm

--- player.py
+++ player.py
@@ -42,3 +42,50 @@ class HumanPlayer(Player):
             except ValueError:
                 print('Invalid square. Try again.')
         return val
+    
+class SmartComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            square = random.choice(game.available_moves())
+        else:
+            square = self.minimax(game, self.letter)['position']
+        return square
+    
+    # decision making algorithm
+    def minimax(self, state, player):
+        max_player = self.letter  # yourself
+        other_player = 'O' if player == 'X' else 'X'
+
+        # first we want to check if the previous move is a winner
+        if state.current_winner == other_player:
+            return {'position': None, 'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (
+                        state.num_empty_squares() + 1)}
+        elif not state.empty_squares():
+            return {'position': None, 'score': 0}
+
+        if player == max_player:
+            best = {'position': None, 'score': -math.inf}  # each score should maximize
+        else:
+            best = {'position': None, 'score': math.inf}  # each score should minimize
+        
+        for possible_move in state.available_moves():
+            # make a move, try that spot
+            state.make_move(possible_move, player)
+            # recurse using minimax to simulate a game after making that move
+            sim_score = self.minimax(state, other_player)
+
+            # undo move
+            state.board[possible_move] = ' '
+            state.current_winner = None
+            sim_score['position'] = possible_move
+
+            if player == max_player:  # maximizing max_player
+                if sim_score['score'] > best['score']:
+                    best = sim_score
+            else:  # minimizing other_player
+                if sim_score['score'] < best['score']:
+                    best = sim_score
+        return best
